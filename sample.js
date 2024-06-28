80d73272a56b8294fbbe0992aa518a4c8a487b57
kishore200111 <kishorekumar.s@proglint.com>
2024-06-28 15:42:07 +0530

    sample Project

---
+++ sample.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, ScrollView, Image,Button} from 'react-native';
+// import { tailwind } from 'tailwindcss-react-native';
+
+
+const name="Ramesh"
+
+const add=(num1,num2)=>{
+    return num1+num2;
+}
+const Students=props=>{
+    return <Text>Hi {props.names}!!, Welcome to React Native Tutorials!!!!!!!!!!</Text>
+}
+
+const Start = () => {
+    const [isActive,setIsActive]=useState(false)
+    return (
+
+        <View style={{backgroundColor:'#00dcff'}}>
+            <Text style={{textAlign:'center',fontSize:25,fontWeight:'500'}}>Welcome to React Js!!</Text>
+            <Text style={{textAlign:'center',margin:'3',}}>{name}</Text>
+            <Text>Sum of Two Numbers:{add(5,5)}</Text>
+            <Image
+                // source={{uri:"https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=600"}} 
+                source={require('./app/assets/sampleimage.jpg')}
+                resizeMode='center'
+                style={{ width: 300, height: 400, borderRadius: 50 }} />
+            <Students names="Guna"/>
+            <Students names="Fayas"/>
+            <Students names="Raghul"/>
+            <Students names="Baga"/>
+            <TextInput
+            style={{height:40,alignItems:'center',borderWidth:2,margin:10}}
+            defaultValue="Enter Messages Here..."
+            />
+            <Text>{isActive?'Plan Activated':'Plan Not Activated'}</Text>
+            <Button onPress={()=>{setIsActive(true)
+            }}title="Activate"/>
+        </View>
+
+    );
+}
+
+export default Start;
